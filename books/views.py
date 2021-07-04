ce799accb3ce2d0d88e4617a0808b805937a5b38
sherchansachin <sachinsherchan3@gmail.com>
2021-07-04 23:57:31 +0545

    templates and routing done

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
+from django.db import models
 from django.shortcuts import render
 
+from django.views.generic import ListView
+from .models import Book
+
 # Create your views here.
+
+# class based views
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
